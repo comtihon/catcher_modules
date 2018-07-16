1ad2657b086dca3d3a2904d63fbe50f637acbd3d
Valerii Tikhonov <com.tihon@mail.ru>
2018-07-16 20:49:47 +0200

    remove autodoc opts

--- docs/conf.py
+++ docs/conf.py
@@ -92,7 +92,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-autodoc_default_flags = ['special-members', 'private-members', 'show-inheritance']
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
