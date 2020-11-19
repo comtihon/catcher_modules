e86267436c544ffd666bcd2a5cc4cc2b8863f2e5
Ekaterina Belova <ek.d.belova@gmail.com>
2020-11-19 16:32:35 +0100

    add raise OldAirflowVersionException where needed

--- catcher_modules/pipeline/airflow_utils/airflow_client.py
+++ catcher_modules/pipeline/airflow_utils/airflow_client.py
@@ -8,6 +8,10 @@ from requests import request
 def trigger_dag(aiflow_url, dag_id, dag_config):
     url = posixpath.join(aiflow_url, 'api/experimental/dags/{}/dag_runs'.format(dag_id))
     r = request('POST', url, json=dag_config, headers={'content-type': 'application/json'})
+    if r.status_code == 404:  # old airflow, rest api is not supported
+        debug('Endpoint not found: ' + r.text)
+        raise OldAirflowVersionException('Can\'t trigger dag {}'.format(dag_id))
+
     if r.status_code != 200:
         raise Exception('Can\'t trigger dag: {}'.format(r.json()))
     debug(r.json())
@@ -23,7 +27,7 @@ def trigger_dag(aiflow_url, dag_id, dag_config):
 def unpause_dag(aiflow_url, dag_id):
     url = posixpath.join(aiflow_url, 'api/experimental/dags/{}/paused/false'.format(dag_id))
     r = request('GET', url)
-    if r.status_code == 404:  # old airflow, rest api is not suported
+    if r.status_code == 404:  # old airflow, rest api is not supported
         debug('Endpoint not found: ' + r.text)
         raise OldAirflowVersionException('Can\'t unpause the dag {}'.format(dag_id))
     if r.status_code != 200:
@@ -34,18 +38,28 @@ def unpause_dag(aiflow_url, dag_id):
 def get_dag_run(aiflow_url: str, dag_id: str, run_id: str) -> dict:
     url = posixpath.join(aiflow_url, 'api/experimental/dags/{}/dag_runs'.format(dag_id))
     r = request('GET', url)
+    if r.status_code == 404:  # old airflow, rest api is not supported
+        debug('Endpoint not found: ' + r.text)
+        raise OldAirflowVersionException('Can\'t get dag run {}'.format(dag_id))
     if r.status_code != 200:
         raise Exception('Can\'t get list of run: {}'.format(r.json()))
     debug(r.json())
     dag_run = filter(lambda x: x['run_id'] == run_id, r.json())
     if not dag_run:
         raise Exception('No run_id {} found in runs for dag {}'.format(run_id, dag_id))
+    # TODO potential issue: will always return the same run_id the last one or the first one ???
     return list(dag_run)[0]
 
 
 def get_run_status(aiflow_url: str, dag_id: str, execution_date: str) -> str:
+    # TODO Potential issue dag can have multiplied runs, unclear, which one will be returned here
+    #  or dag_id == run_id ?
     url = posixpath.join(aiflow_url, 'api/experimental/dags/{}/dag_runs/{}'.format(dag_id, execution_date))
     r = request('GET', url)
+    if r.status_code == 404:  # old airflow, rest api is not supported
+        debug('Endpoint not found: ' + r.text)
+        raise OldAirflowVersionException('Can\'t get dag run status {}'.format(dag_id))
+
     if r.status_code != 200:
         raise Exception('Can\'t get run status for {}:{}  {}'.format(dag_id, execution_date, r.json()))
     return r.json()['state']
